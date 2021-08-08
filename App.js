51fabd2768b5edc380e7f9bac3e62cbbd7c5238a
Marwen46 <marwen.ayoub@outlook.com>
2021-08-08 21:45:52 +0100

    make some small fixes

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {I18nManager} from 'react-native';
 import SignIn from './Screens/SignIn';
 import SignInNextStep from './Screens/SignInNextStep';
 import Subscribe from './Screens/Subscribe';
+import SplashScreen from 'react-native-splash-screen';
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   I18nManager.forceRTL(true);
   return <Subscribe />;
 };
